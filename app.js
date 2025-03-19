0843d61bd1af25755b68d95fc9627ba5387c5f66
DhavasheelanG04 <dhavajaya8283@gmail.com>
2025-03-19 16:07:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var on_btn=document.getElementById("on");
+var off_btn=document.getElementById("off");
+var img=document.getElementById("bulb");
+
+function bulb_on(){
+    img.src="https://cdn.pixabay.com/photo/2014/04/03/10/32/bulb-310821_640.png";
+}
+
+function bulb_off(){
+    img.src="https://cdn.pixabay.com/photo/2013/07/12/14/34/filament-148482_640.png";
+}
